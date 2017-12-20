6fc2cbb2048e6df54d89ef0cacffd14564905283
Admin <Admin@macs-MacBook-Pro-2.local>
2017-12-20 17:16:53 +0000

    API completed. Added CORS

--- config/app.php
+++ config/app.php
@@ -164,6 +164,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
+
         /*
          * Package Service Providers...
          */
@@ -176,6 +178,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        \Barryvdh\Cors\ServiceProvider::class,
 
     ],
 
